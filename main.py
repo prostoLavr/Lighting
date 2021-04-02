712d44f6a021260adc7dbbe2024da2dca0c06dd8
lawrence <prostoLavr@mail.tu>
2021-04-02 22:01:30 +0800

    Add graphic for button and other updates

--- main.py
+++ main.py
@@ -20,28 +20,27 @@ class MySerial:
         pass
 
     @staticmethod
-    def send(text):
-        print('port send:', text)
+    def send(pin, text):
+        print('port send:', pin, text)
 
 
 class Gui(QWidget):
-    def __init__(self):
+    def __init__(self, serial):
         super().__init__()
-        self.init_ui()
+        self.init_ui(serial)
 
-    def init_ui(self):
+    def init_ui(self, serial):
         # self.setGeometry() потом можно использовать его
         self.setFixedSize(WIDTH, HEIGHT)
-        l1 = LightButton(self, 20)
-        l2 = LightButton(self, 70 + SUN_SIZE)
+        LightButton(0, serial, 20, self)
+        LightButton(1, serial, 70 + SUN_SIZE, self)
         self.show()
-
-    def button_click(self):
-        print('click', self)
 
 
 class LightButton:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, pin, serial, *args, **kwargs):
+        self.pin = pin
+        self.serial = serial
         self.label = None
         self.is_on = False
         self.pixmap_on = QPixmap('sun_on.png')
@@ -52,7 +51,7 @@ class LightButton:
     def clicked_button(self):
         print(self)
 
-    def draw_me(self, my_parent, x_coord):
+    def draw_me(self, x_coord, my_parent):
         y_coefficient = 50
         self.label = QLabel(my_parent)
         self.label.setPixmap(self.pixmap_off)
@@ -65,11 +64,12 @@ class LightButton:
 
     def __call__(self):
         self.is_on = not self.is_on
+        self.serial.send(self.pin, self.is_on)
         self.label.setPixmap(self.pixmap_on if self.is_on else self.pixmap_off)
-        print(f'I ({self}) is {"on" if self.is_on else "off"}')
 
 
 if __name__ == '__main__':
+    my_serial = MySerial()
     app = QApplication(sys.argv)
-    master = Gui()
+    master = Gui(my_serial)
     sys.exit(app.exec())
